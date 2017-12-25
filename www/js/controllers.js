e6fe8be8d95eef8cc7a1ed6d465daa6d43a59bf2
JJ Berrett <developer@jjberrett.com>
2017-12-24 20:26:47 -0700

    chore: build ios deployment package
    All services and controlers are now functioning and app is ready for deployment

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,169 +1,182 @@
 angular.module('controllers',[])
 
-  .controller('HomeCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope, $ionicPopup, restaurantSrvc) {
+	  $scope.showAlert = function() {
+	  	let randomRestaurant = restaurantSrvc.getRandom();
 
+	  	$ionicPopup.alert({
+			  title: `Maybe ${ randomRestaurant.name }?`,
+			  cssClass: 'popupBox'
+		  });
+	  };
   })
 
-  .controller('restaurantCtrl', function ($scope, restaurantSrvc, ionicToast){
-    let restaurants;
-    $scope.restaurants = {};
-
-    function mapRestaurants () {
-      $scope.loading = true;
-      restaurantSrvc.getRestaurants()
-      .then((result) => {
-        restaurants = result.data;
-        let restaurantTypes = [];
-
-        //Find all the different types of restaurants
-        restaurants.map((restaurant) => {
-          let found = restaurantTypes.find(({type}) => {
-            return type === restaurant.type;
-          })
-
-          if (!found) {
-            restaurantTypes.push({
-              type: restaurant.type,
-              restaurants: []
-            });
-          }
-          let index = restaurantTypes.findIndex(i => i.type === restaurant.type)
-          restaurantTypes[index].restaurants.push(restaurant);
-        });
-
-        $scope.mappedRestaurantTypes = restaurantTypes;
-        $scope.loading = false;
-      });
-    }
-    mapRestaurants();
+  .controller('restaurantCtrl', function ($scope, restaurantSrvc){
+	  $scope.$on( '$ionicView.enter', () => {
+		  let restaurants = restaurantSrvc.getRestaurants();
+		  return mapRestaurants($scope, restaurants);
+	  });
   })
 
-  .controller('chewsCtrl', function ($scope, restaurantSrvc, ionicToast) {
-    // $scope.sendSms = function () {
-    //   let message = 'My Schedule \n \n';
-    //   schedule = sessionsSrvc.getSchedule();
-    //   if (schedule) {
-    //     for (let i = 0; i < schedule.length; i++) {
-    //       message = message.concat("Breakout " + (i + 1) + ": " + schedule[i].title + "\n \n");
-    //     }
-    //     let options = {
-    //       replaceLineBreaks: false, // true to replace \n by a new line, false by default
-    //       android: {
-    //         intent: 'INTENT'  // send SMS with the native android SMS messaging
-    //         //intent: '' // send SMS without open any other app
-    //       }
-    //     };
-    //     let number = '';
-    //     sms.send(number, message, options);
-    //   }
-    //   else {
-    //     ionicToast.show('Nothing in your schedule, please add before sharing', 'middle', false, 2500);
-    //   }
-    // };
-    //
-    //
-    // function getSchedule() {
-    //   let scheduledSessions = sessionsSrvc.getSchedule();
-    //   if (scheduledSessions) {
-    //     $scope.scheduledSessions = scheduledSessions;
-    //     $scope.noSchedule = false;
-    //   }
-    //   else {
-    //     $scope.noSchedule = true;
-    //   }
-    // }
-    //
-    // $scope.removeFromSchedule = function (id) {
-    //   sessionsSrvc.removeFromSchedule(id);
-    //   getSchedule();
-    // };
-    //
-    // $scope.$on('$ionicView.enter', function (e) {
-    //   getSchedule()
-    // });
-  })
+  .controller('chewsCtrl', function ($scope, restaurantSrvc, $ionicPopup, $location) {
+	  let restaurantList = [];
+	  let newList;
+	  let picked;
+	  let final;
 
-  .controller('restaurantDetailCtrl', function ($scope, restaurantSrvc, $stateParams, ionicToast) {
-   $scope.restaurant = restaurantSrvc.getRestaurant($stateParams.id);
+	  let finalPopup = (restaurant) => {
+		  let myPopup = $ionicPopup.show({
+			  title: `${ restaurant.name } sounds great!`,
+			  cssClass: 'popupBox',
+			  buttons: [
+				  {
+					  text: 'Ok',
+					  type: 'button-positive',
+				  }
+			  ]
+		  });
+
+		  myPopup.then(() => {
+			  $location.path('/tab/home');
+		  });
+	  };
+
+	  let finalList = () => {
+		  let myPopup = $ionicPopup.show({
+			  title: "<p class='positive'>Here is your final list! Pick where you want to go!</p>",
+			  buttons: [
+				  {
+					  text: 'Ok',
+					  type: 'button-positive',
+					  onTap: function(e) {
+					  	mapRestaurants($scope, newList);
+					  }
+				  }
+			  ]
+		  });
+	  };
+
+	  let myapp = () => {
+		  if (restaurantList.length >= 5 && picked !== 4) {
+			  let restaurants = [];
+
+			  for(let i = 0; i < 5; i++){
+				  let index = Math.floor(Math.random() * restaurantList.length);
+				  let restaurant = restaurantList.splice(index, 1);
+				  restaurants.push(restaurant[0]);
+			  }
+
+			  mapRestaurants($scope, restaurants);
+		  }
+		  else {
+		  	final = true;
+			  finalList();
+		  }
+	  };
+
+	  let showPopup = () => {
+		  let myPopup = $ionicPopup.show({
+			  title: "<p class='positive'>Pick a couple of Restaurants from each list</p>",
+			  buttons: [
+				  {
+				  	text: 'Ok',
+					  type: 'button-positive',
+				  }
+			  ]
+		  });
+
+		  myPopup.then(() => {
+			  let restaurants = restaurantSrvc.getRestaurants();
+			  for (let i = 0; i < restaurants.length; i++) {
+				  restaurantList.push(restaurants[i]);
+			  }
+			  newList = [];
+			  picked = 0;
+			  final = false;
+			  myapp();
+		  });
+	  };
+
+	  $scope.$on( '$ionicView.enter', () => {
+	  	showPopup();
+	  });
+
+	  $scope.pickedRestaurant = (restaurant) => {
+	  	if(final){
+	  		finalPopup(restaurant);
+		  }
+		  else {
+			  newList.push(restaurant);
+			  picked++;
+			  myapp();
+		  }
+	  }
   })
 
 	.controller('addRestaurantCtrl', function ($scope, restaurantSrvc, $stateParams) {
-		// $scope.session = sessionsSrvc.getSession($stateParams.id);
-		// $scope.submitReview = function (session) {
-    //
-		// 	if (session.likeFeedback || session.dislikeFeedback || session.generalFeedback) {
-		// 		var review = {
-		// 			sessionId: session.id,
-		// 			sessionTitle: session.title,
-		// 			sessionSpeaker: session.speaker,
-		// 			userName: session.userName || "",
-		// 			userEmail: session.userEmail || "",
-		// 			likeFeedback: session.likeFeedback || "",
-		// 			dislikeFeedback: session.dislikeFeedback || "",
-		// 			generalFeedback: session.generalFeedback || "",
-		// 			rating : session.rating || ""
-		// 		};
-    //
-		// 		sessionsSrvc.submitReview(review)
-		// 	}
-		// };
+		$scope.master = {};
 
-    $scope.addRestaurant = (restaurant) => {
+		$scope.addRestaurant = (restaurant) => {
       if(restaurant.name !== '') {
-        restaurantSrvc.addRestaurant(restaurant);
+        restaurantSrvc.createRestaurant(restaurant);
       }
       else {
-        console.log("Ruh roh, I couldn't create a restaurant");
+        $scope.restaurant = angular.copy($scope.master);
       }
     }
 	})
 
-.controller('removeRestaurantCtrl', function ($scope, restaurantSrvc, $stateParams) {
-	// let sessionLength = 0;
-	// let sessionType;
-	// let sessions;
-	// $scope.sessions = {};
-  //
-	// function getAllSessions() {
-	// 	$scope.loading = true;
-	// 	sessionsSrvc.getSessions()
-	// 		.then((res) => {
-	// 			sessions = res.data;
-	// 			sessionsSrvc.setSessions(sessions);
-	// 			return sessions;
-	// 		})
-	// 		.then(function (result) {
-	// 			let mappedSessions = [];
-	// 			sessions = result;
-  //
-	// 			//Create Mapped array with sessionType, SessionTime, then array of sessions
-	// 			result.map(function (session) {
-	// 				let isMapped = mappedSessions.find(function (mappedSession) {
-	// 					return mappedSession.sessionType === session.sessiontype;
-	// 				});
-  //
-	// 				if (!isMapped) {
-	// 					mappedSessions.push({
-	// 						sessionType: session.sessiontype,
-	// 						sessionTime: session.sessiontime,
-	// 						sessions: []
-	// 					})
-	// 				}
-	// 			});
-  //
-	// 			//Add sessions to the session object in the mapped session.
-	// 			result.map(function (session) {
-	// 				let mappedSession = mappedSessions.find(function (mappedSession) {
-	// 					return mappedSession.sessionTime === session.sessiontime;
-	// 				});
-	// 				if (mappedSession) {
-	// 					mappedSession.sessions.push(session);
-	// 				}
-	// 			});
-	// 			$scope.mappedSessions = mappedSessions;
-	// 			$scope.loading = false;
-	// 		})
-	// }
-  //
-	// getAllSessions()
+.controller('removeRestaurantCtrl', function ($scope, restaurantSrvc, $ionicPopup, $location) {
+	let restaurants = restaurantSrvc.getRestaurants();
+	mapRestaurants($scope, restaurants);
+
+	$scope.removeRestaurant = (restaurant) => {
+			let confirmPopup = $ionicPopup.confirm({
+				title: 'Delete Restaurant',
+				template: `Are you sure you want to delete ${restaurant.name}?`
+			});
+
+			confirmPopup.then((res) => {
+				if(res) {
+					restaurantSrvc.deleteRestaurant(restaurant);
+					mapRestaurants($scope, restaurants);
+					$location.path('/tab/manage');
+				}
+			});
+		};
 });
+
+function mapRestaurants ($scope, restaurants) {
+	$scope.loading = true;
+	if(!restaurants) {
+		restaurants = restaurantSrvc.getRestaurants();
+	}
+	let restaurantTypes = [];
+
+	restaurants.map((restaurant) => {
+		let found = restaurantTypes.find(({type}) => {
+			return type === restaurant.type;
+		});
+
+		if (!found) {
+			restaurantTypes.push({
+				type: restaurant.type,
+				restaurants: []
+			});
+		}
+		let index = restaurantTypes.findIndex(i => i.type === restaurant.type);
+		restaurantTypes[index].restaurants.push(restaurant);
+	});
+
+	$scope.mappedRestaurantTypes = restaurantTypes;
+	$scope.loading = false;
+}
+
+
+//================ For V2 =================//
+
+// .controller('restaurantDetailCtrl', function ($scope, $sce, restaurantSrvc, $stateParams, ionicToast) {
+//  $scope.restaurant = restaurantSrvc.getRestaurant($stateParams.id);
+// })
+
+
--- www/js/services.js
+++ www/js/services.js
@@ -2,75 +2,113 @@ angular.module('services', [])
   .service("restaurantSrvc", function ($http, $localStorage) {
 
     if (!$localStorage.restaurants) {
-      $localStorage.restaurants = [];
+      $localStorage.restaurants = [
+      	{"name": "Chili's", "type": "Sit-down"},
+	      {"name": "Outback", "type": "Sit-down"},
+	      {"name": "The Habit", "type": "Hamburgers"},
+	      {"name": "Pizza Factory", "type": "Pizza"},
+	      {"name": "Chick-fil-a", "type": "Fast-food"},
+	      {"name": "Wendy's", "type": "Fast-food"},
+	      {"name": "Durangos", "type": "Mexican"},
+	      {"name": "AppleBee's", "type": "Sit-down"},
+	      {"name": "Texas Roadhouse", "type": "Sit-down"},
+	      {"name": "Subway", "type": "Fast-food Healthy"},
+	      {"name": "Red-Lobster", "type": "Sea-food"},
+	      {"name": "Panda Express", "type": "Fast-Food"},
+	      {"name": "Cafe Rio", "type": "Mexican"},
+	      {"name": "Don Pedro's", "type": "Mexican"},
+	      {"name": "Olive Garden", "type": "Italian"}
+	      ];
     }
 
     this.createRestaurant = (restaurant) => {
-      getRestaurants()
-      .then((res) => {
-        if (res.status !== 500) {
-          let existingRestaurant = res.data.find(function (existingRest) {
-            return existingRest.name === restaurant.name
-          });
+	    let existingRestaurant = $localStorage.restaurants.find((existingRest) => {
+	      return existingRest.name === restaurant.name;
+      });
 
-          if (!existingRestaurant)
-            return $http.post('Http://localhost:8085/api/restaurant', restaurant)
-            .then((res) => {
-              if (res.status !== 500) {
-                $localStorage.restaurants.push(res.data.name);
-                return { status: res.status, message: `Added Restaurant: ${res.data.name} successfully!`, data: null};
-              }
-              else {
-                return {
-                  status: res.status,
-                  response: {
-                    message: `There was an error creating restaurant. Close the app and try again.`,
-                    data: null
-                  }
-                };
-              }
-            });
-          return {status: 409, message: `${ restaurant.name } already exists!`, data: null};
-        }
-      })
-    }
+	    if (!existingRestaurant) {
+		    $localStorage.restaurants.push(restaurant);
+		    return true;
+      }
+      return false;
+
+      //TODO: Uncomment this for v2
+      // getRestaurants()
+      // .then((res) => {
+      //   if (res.status !== 500) {
+      //     let existingRestaurant = res.data.find(function (existingRest) {
+      //       return existingRest.name === restaurant.name
+      //     });
+      //
+      //     if (!existingRestaurant)
+      //       return $http.post('https://helpmechews.herokuapp.com/api/restaurant', restaurant)
+      //       .then((res) => {
+      //         if (res.status !== 500) {
+      //           $localStorage.restaurants.push(res.data.name);
+      //           return { status: res.status, message: `Added Restaurant: ${res.data.name} successfully!`, data: null};
+      //         }
+      //         else {
+      //           return {
+      //             status: res.status,
+      //             response: {
+      //               message: `There was an error creating restaurant. Close the app and try again.`,
+      //               data: null
+      //             }
+      //           };
+      //         }
+      //       });
+      //     return {status: 409, message: `${ restaurant.name } already exists!`, data: null};
+      //   }
+      // })
+    };
 
     this.getRestaurants = () => {
-      return $http.get('Http://localhost:8085/api/restaurants')
-      .then((res) => {
-        if (res.status !== 500) {
-          $localStorage.restaurants = res.data;
-          return {status: res.status, message: null, data: res.data};
-        }
-        else {
-          return {
-            status: res.status,
-            response: {message: `There was an error getting the restaurants. Close the app and try again.`},
-            data: res
-          };
-        }
-      });
-    }
+      return $localStorage.restaurants;
+      //TODO: Uncomment this for V2
+      // return $http.get('https://helpmechews.herokuapp.com/api/restaurants')
+      // .then((res) => {
+      //   if (res.status !== 500) {
+      //     $localStorage.restaurants = res.data;
+      //     return {status: res.status, message: null, data: res.data};
+      //   }
+      //   else {
+      //     return {
+      //       status: res.status,
+      //       response: {message: `There was an error getting the restaurants. Close the app and try again.`},
+      //       data: res
+      //     };
+      //   }
+      // });
+    };
 
-    this.getRestaurant = (id) => {
-      return $localStorage.restaurants.find(function (restaurant) {
-        return restaurant.id == parseInt(id);
-      })
-    }
+    //TODO: Uncomment for v2
+    // this.getRestaurant = (id) => {
+    //   return $localStorage.restaurants.find(function (restaurant) {
+    //     return restaurant.id === id;
+    //   })
+    // };
 
-    this.deleteRestaurant = (id) => {
-      return $http.post('http://localhost:8085/api/restaurant', id)
-      .then((res) => {
-        if (res.status !== 500) {
-          return {status: res.status, response: {message: 'Deleted Successfully', data: null}};
-        }
-        else {
-          return {
-            status: res.status,
-            response: {message: `There was an error deleting the restaurant. Close the app and try again.`},
-            data: res
-          };
-        }
-      });
+	  this.getRandom = () => {
+	  	let index = Math.floor(Math.random() * $localStorage.restaurants.length);
+			return $localStorage.restaurants[index];
+
+	  };
+
+    this.deleteRestaurant = (name) => {
+	    $localStorage.restaurants = $localStorage.restaurants.filter(n => n !== name);
+      //TODO: Uncomment for v2
+      // return $http.post('https://helpmechews.herokuapp.com/api/restaurant', id)
+      // .then((res) => {
+      //   if (res.status !== 500) {
+      //     return {status: res.status, response: {message: 'Deleted Successfully', data: null}};
+      //   }
+      //   else {
+      //     return {
+      //       status: res.status,
+      //       response: {message: `There was an error deleting the restaurant. Close the app and try again.`},
+      //       data: res
+      //     };
+      //   }
+      // });
     }
   });
